f0d2c79a15d0daa321adafec64cbc1cd097ecf54
Jack Slater <jack@Jacks-MacBook-Pro.local>
2017-05-04 10:02:16 +0530

    Mommifier does not mommify empty string

---
+++ src/mommy/Mommifier.java
@@ -0,0 +1,11 @@
+package mommy;
+
+/**
+ * Created by jack on 04/05/2017.
+ */
+public class Mommifier {
+
+    public String mommify(String word) {
+        return "";
+    }
+}
---
+++ test/mommy/MommifyTest.java
@@ -0,0 +1,17 @@
+package mommy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by jack on 04/05/2017.
+ */
+public class MommifyTest {
+
+    @Test
+    public void shouldReturnAnEmptyStringWhenAnEmptyStringIsGiven () {
+        Mommifier mommifier = new Mommifier();
+        assertEquals("", mommifier.mommify(""));
+    }
+}
