fcf5b18d815384f9b9d5a8c995bfdb4afb24a48f
Joe Ray <jray@thoughtworks.com>
2017-05-11 22:43:39 +0530

    [Joe] Add feedback

--- src/mommy/Mommifier.java
+++ src/mommy/Mommifier.java
@@ -4,13 +4,17 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Mommifier {
-
+    // TODO Good use of final, but do they need to be instance variables?
     private final double vowelLimit = 0.3;
     private final List<Character> vowels = Arrays.asList('a', 'e', 'i', 'o', 'u');
 
     public String mommify(String word) {
         String result = "";
         int wordLength = word.length();
+
+        // TODO I'm generally not a fan of if statements without braces, I find them more difficult to read. However,
+        // there's another structure you could use here to make the end of this method simpler. Have you use the ternary
+        // operator before? There's also another place in this file where you could use it.
         if (checkVowelCount(word, wordLength)) return word;
         return mommifyVowels(word, result, wordLength);
     }
@@ -45,7 +49,6 @@ public class Mommifier {
         return result;
     }
 
-    // TODO If you don't need to use the methods on the Boolean class, it's probably better to stick to the primitive
     private boolean previousLetterAVowel(String word, int letterPosition) {
         return letterPosition != 0 && vowels.contains(word.charAt(letterPosition - 1));
     }
