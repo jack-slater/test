ea7b04b47c703cc95a3a50e5fc273489abc232fe
Jack Slater <jslater@thoughtworks.com>
2017-05-14 21:21:24 +0530

    [Jack] refactor mommify and mommifyVowels to use tertiary statement

--- src/mommy/Mommifier.java
+++ src/mommy/Mommifier.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Mommifier {
-    // TODO Good use of final, but do they need to be instance variables?
+
     private final double vowelLimit;
     private final List<Character> vowels;
 
@@ -16,22 +16,13 @@ public class Mommifier {
     public String mommify(String word) {
         String result = "";
         int wordLength = word.length();
-
-        // TODO I'm generally not a fan of if statements without braces, I find them more difficult to read. However,
-        // there's another structure you could use here to make the end of this method simpler. Have you use the ternary
-        // operator before? There's also another place in this file where you could use it.
-        if (checkVowelCount(word, wordLength)) return word;
-        return mommifyVowels(word, result, wordLength);
+        return checkVowelCount(word, wordLength) ? word : mommifyVowels(word, result, wordLength);
     }
 
     private String mommifyVowels(String word, String result, int wordLength) {
         for (int i = 0; i < wordLength; i++) {
             char letter = word.charAt(i);
-            if (vowels.contains(letter)) {
-                result = mommifyVowel(word, result, i);
-            } else {
-                result += letter;
-            }
+            result = vowels.contains(letter) ? mommifyVowel(word, result, i) : result + letter;
         }
         return result;
     }
