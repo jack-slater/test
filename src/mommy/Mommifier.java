205c53a5f5f5042b9d77ce8a881edb7c7abbf2ef
Jack Slater <jslater@thoughtworks.com>
2017-05-29 18:58:01 +0530

    [Jack] change Mommifier variables to static

--- src/mommy/Mommifier.java
+++ src/mommy/Mommifier.java
@@ -4,16 +4,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Mommifier {
-    // TODO I think maybe you misunderstood me here. You've still implemented an instance variable, just instantiated in
-    // the constructor which is unnecessary. What I meant was instead of an instance variable, should this be a class
-    // variable? They should be constants which in Java are implemented using `<accessor> static final`
-    private final double vowelLimit;
-    private final List<Character> vowels;
 
-    public Mommifier () {
-        vowelLimit = 0.3;
-        vowels = Arrays.asList('a', 'e', 'i', 'o', 'u');
-    }
+    private static final double vowelLimit = 0.3;
+    private static final List<Character> vowels = Arrays.asList('a', 'e', 'i', 'o', 'u');
 
     public String mommify(String word) {
         String result = "";
